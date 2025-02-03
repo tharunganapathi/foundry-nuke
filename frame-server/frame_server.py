f35c3d22c109d69ecc1bc2b9ea2c2054b0d0731a
TharunG <65713157+tharunganapathi@users.noreply.github.com>
2025-02-03 20:53:51 +0530

    Update frame_server.py

--- frame-server/frame_server.py
+++ frame-server/frame_server.py
@@ -2,11 +2,11 @@ print '\n'
 from hiero.ui.nuke_bridge.FnNsFrameServer import frameServer
 s = [worker.address for worker in frameServer.getStatus(1).workerStatus]
 
-
+# print the workers
 for i in s:
-print i
+  print (i)
 
 
 
-
+# execute
 python.exe "C:\Program Files\Nuke12.2v5\pythonextensions\site-packages\foundry\frameserver\nuke\runframeserver.py" --numworkers=2 --nukeworkerthreads=4 --nukeworkermemory=8096 --workerconnecturl=tcp://192.168.5.26:5560 --nukepath=Nuke12.2.exe
